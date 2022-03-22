75163435dbc622aaaca7fdddadd89e4be970c918
Conrado Bolonha <conradobolonha@gmail.com>
2022-03-22 17:45:24 -0300

    exerc.v01

--- Aula009/aula009.py
+++ Aula009/aula009.py
@@ -12,4 +12,27 @@ frase3 = 'Frase do dia com salto de dois em dois'
 print(frase3[0:41:2])
 
 frase4 = 'Frase do dia com salto de três em três'
-print(frase4[0:100:3])
+print(frase4)
+print(len(frase4))
+print(frase4[0:38:3])
+
+# Análise de uma string
+# len = conta a quantidade de caracter
+frase5 = 'Frase do dia usando a função "count"'
+print (len(frase5))
+
+# count = Contas quantas letras "e" a frase tem.
+frase6 = 'Meus estudo em Python'
+print(frase6.count('e'))
+
+# Conta a quantidade da letra "e" do caracter 0 ao 13.
+frase7 = 'Meu estudo em Python vai que vai'
+print(frase7.count('e', 0, 10))
+
+# find = Mostra a posição de um caracter
+frase8 = 'Localizador de letras usando o argumento find'
+print(frase8.find('tras'))
+
+# comando "in" -> Procura caracter(s) e mostra True ou False.
+frase9 = 'Procurando caracter(s) com o comando "in"'
+print('com')
