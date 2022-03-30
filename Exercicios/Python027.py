0720e4316fb5b523841ce934e3a1d9f254d74ceb
Conrado B <86488868+ConradoBolonha@users.noreply.github.com>
2022-03-30 17:46:26 -0300

    Update Python027.py

--- Exercicios/Python027.py
+++ Exercicios/Python027.py
@@ -1,5 +1,5 @@
 """
 Faça um programa que leia o nome completo de uma pessoa, mostrando
-em seguida o primeiro e o último nome separadamente
+em seguida o primeiro e o último nome separadamente.
 
 """
