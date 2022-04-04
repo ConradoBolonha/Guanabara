b2bee5add6cc3d2c4ddcf05508e26de2181b807f
Conrado Bolonha <conradobolonha@gmail.com>
2022-04-04 17:41:03 -0300

    v.1.00

--- Aula009/temp_009-1.py
+++ Aula009/temp_009-1.py
@@ -1,16 +1,12 @@
 """
-Crie um programa que leia o nome completo de uma pessoa e mostre:
+Faça um programa que crie um número de 0 a 9999 e
+mostre na tela cada um dos digitos separados.
+Ex: Número 1529
+unidade: 9
+dezena: 2
+centena: 5
+milhar: 1
 
-1-O nome completo com todas as letras maiúscula;
-2-O nome com todas minúsculas;
-3-Quantas letras ao todo (sem considerar espaços);
-4-Quantas letras tem o primeiro nome.
+Obs: Tente fazer como String e matematicamente
 """
 
-nome = str(input('Digite seu nome: ')).strip()
-#  dividir = nome.split()
-print('Seu nome em maiúsculo é {}'.format(nome.upper()))
-print('Seu nome em minúsculo é {}'.format(nome.lower()))
-print('Seu nome completo tem {}'.format(len(nome) - nome.count(' ')))
-#  print('Seu primeiro nome tem {}'.format(len(dividir[0])))
-print('Seu primeiro nome tem {}'.format(nome.find(' ')))
