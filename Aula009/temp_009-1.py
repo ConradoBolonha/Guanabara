6a7a95b9109865a4615d64c7f5353aecf704a43f
Conrado Bolonha <conradobolonha@gmail.com>
2022-04-06 17:46:19 -0300

    v.01.00

--- Aula009/temp_009-1.py
+++ Aula009/temp_009-1.py
@@ -8,6 +8,7 @@ Crie um programa que leia o nome completo de uma pessoa e mostre:
 """
 
 nome = str(input('Digite seu nome: '))
-print(nome.upper())
-print(nome.lower())
-print(len(nome) - nome.count(' '))
+print('Seu nome com letras maiúscula é {}'.format(nome.upper()))
+print('Seu nome com letras minúscula é {}'.format(nome.lower()))
+print('Seu nome tem {} letras'.format(len(nome) - nome.count(' ')))
+print('Seu primeiro nome tem {}'.format(nome.find(' ')))
