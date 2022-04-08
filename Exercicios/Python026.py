20c8569d8bea93a3394b9136a794953a4063a819
Conrado Bolonha <conradobolonha@gmail.com>
2022-04-08 17:41:59 -0300

    v.1.00

--- Exercicios/Python026.py
+++ Exercicios/Python026.py
@@ -6,7 +6,7 @@ Crie um algoritmo que leia uma frase pelo teclado e mostre:
 3-Em que posição ela aparece a última vez.
 """
 
-frase = (         'Amanhã, com certeza irA chover'         ).strip()
+frase = (         'Amanhã, com certeza irA chover'          ).strip()
 print(frase)
 print('Na string, contém {} letra "A".'.format(frase.count('A')))
 print('A primeira letra "A" está na posição {}.'.format(frase.find('A') + 1))
