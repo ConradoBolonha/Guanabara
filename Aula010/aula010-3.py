10fa53f980ea8c9a057f1d1414189579615c1d18
Conrado Bolonha <conradobolonha@gmail.com>
2022-04-13 16:59:20 -0300

    v.1.00

---
+++ Aula010/aula010-3.py
@@ -0,0 +1,12 @@
+"""
+nome = str(input('Digite seu nome: '))
+if nome == 'Conrado':
+    print('Olá {}. Sim, seu nome é Conrado!'.format(nome))
+else:
+    print('Seu nome NÃO é {}'.format(nome))
+"""
+
+nome = str(input('Digite seu nome: '))
+if nome == 'Conrado':
+    print('Olá {}, seu nome é bonito!'.format(nome))
+print('Olá Sr. {}, como vai?'.format(nome))
