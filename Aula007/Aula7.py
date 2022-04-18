2e97127ebe22bc3537182a2cbebfe0306f067d9e
Conrado B <86488868+ConradoBolonha@users.noreply.github.com>
2022-04-18 13:24:36 -0300

    Delete Aula007 directory

--- Aula007/Aula7.py
+++
@@ -1,9 +0,0 @@
-n1 = 5
-n2 = 2
-print('Adição: ', n1 + n2)
-print('Subtração: ', n1 - n2)
-print('Multiplicação: ', n1 * n2)
-print('Divisão: ', n1 / 2)
-print('Potenciação: ', n1 ** n2)
-print('Divisão inteira: ', n1 // n2)
-print('Resto da divisão: ', n1 % n2)
