f0fe5b22fc2d53224b67763106db762ce7dbae97
Conrado B <86488868+ConradoBolonha@users.noreply.github.com>
2022-04-18 13:24:58 -0300

    Delete Aula009 directory

--- Aula009/temp_009-1.py
+++
@@ -1,9 +0,0 @@
-"""
-Faça um programa que leia o nome completo de uma pessoa, mostrando
-em seguida o primeiro e o último nome separadamente.
-
-"""
-
-nome = input('Digite seu nome completo: ')
-div_nome = nome.split()
-print(len(div_nome[0], [div_nome -1]))
