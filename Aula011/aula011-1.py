d4687e7cc8877799d3cad27d1b498e23ed65cb3c
Conrado Bolonha <conradobolonha@gmail.com>
2022-04-19 14:56:42 -0300

    v.1.00

---
+++ Aula011/aula011-1.py
@@ -0,0 +1,14 @@
+"""
+Colorindo o terminal
+
+***  STYLE  ***                 ***  TEXT  ***              ***  BACK GROUND  ***
+0 - None                        30 - Branco                 40 - Branco
+1 - Bold (negrito)              31 - Vermelho               41 - Vermelho
+4 - Underline (Sublinhado)      32 - Verde                  42 - Verde
+7 - Negative (Inverte)          33 - Amarelo                43 - Amarelo
+                                34 - Azul escuro            44 - Azul Claro
+                                35 - Roxo                   45 - Roxo
+                                36 - Azul claro             46 - Azul claro
+                                37 - Cinza                  47 - Cinza
+
+"""                 
