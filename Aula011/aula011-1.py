e4e14ebd0eb54b45e009e4f4253cdc98b88e7bda
Conrado Bolonha <conradobolonha@gmail.com>
2022-04-19 17:05:44 -0300

    v.1.00

--- Aula011/aula011-1.py
+++ Aula011/aula011-1.py
@@ -1,5 +1,5 @@
 """
-Colorindo o terminal
+=-=-=-=-=-=-=-=  COLORINDO O TERMINAL  =-=-=-=-=-=-=-=
 
       ***  STYLE  ***           ***  TEXT  ***              ***  BACK GROUND  ***
 0 - None                        30 - Branco                 40 - Branco
@@ -24,3 +24,10 @@ s = a + b
 print('A soma de \033[31m{}\033[m e \033[32m{}\033[m é \033[34m{}\033[m'.format(a, b, s))
 nome = 'Conrado'
 print('\nOlá, muito prazer em te conhecer, {}{}{}!'.format('\033[4;34m', nome, '\033[m'))
+
+cores = {'limpa':'\033[m',
+         'azul':'\033[34m',
+         'amarelo':'\033[33m',
+         'pretoebranco':'\033[7:30m'}
+
+print('Agora vamos brincar com variáveis de cores {}!!!'.format(cores['azul'], nome, cores['limpa']))
