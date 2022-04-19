8b64ab7ca73900007bf26941beaa59d6c4eabd45
Conrado Bolonha <conradobolonha@gmail.com>
2022-04-19 16:13:39 -0300

    v.1.00

--- Aula011/aula011-1.py
+++ Aula011/aula011-1.py
@@ -16,3 +16,11 @@ print('\033[31mOlá Mundo!')             # Olá mundo em vermelho.
 print('\033[31;44mOlá Mundo!\033[m')    # Olá mundo com texto vermelho, fundo azul claro só onde tem texto.
 print('\033[1;32;43mOlá Mundo!')        # Olá mundo com texto estilo negrito, texto verde e fundo amarelo.
 print('\033[4;30;45mOlá Mundo!\033[m')  # Olá mundo sublinhado, texto branco e fundo roxo.
+print('\033[7;33;44mOlá Mundo!\033[m')  # 
+
+a = 2
+b = 6
+s = a + b
+print('A soma de \033[31m{}\033[m e \033[32m{}\033[m é \033[34m{}\033[m'.format(a, b, s))
+nome = 'Conrado'
+print('\nOlá, muito prazer em te conhecer, {}{}{}!'.format('\033[4;34m', nome, '\033[m'))
