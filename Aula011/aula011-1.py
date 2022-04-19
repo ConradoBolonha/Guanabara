7661194de605c0211fa70d4a82eaad1bf620a19a
Conrado Bolonha <conradobolonha@gmail.com>
2022-04-19 15:27:33 -0300

    v.1.00

--- Aula011/aula011-1.py
+++ Aula011/aula011-1.py
@@ -1,14 +1,18 @@
 """
 Colorindo o terminal
 
-***  STYLE  ***                 ***  TEXT  ***              ***  BACK GROUND  ***
+      ***  STYLE  ***           ***  TEXT  ***              ***  BACK GROUND  ***
 0 - None                        30 - Branco                 40 - Branco
 1 - Bold (negrito)              31 - Vermelho               41 - Vermelho
 4 - Underline (Sublinhado)      32 - Verde                  42 - Verde
-7 - Negative (Inverte)          33 - Amarelo                43 - Amarelo
+7 - Negative (Inverte a cor)    33 - Amarelo                43 - Amarelo
                                 34 - Azul escuro            44 - Azul Claro
                                 35 - Roxo                   45 - Roxo
                                 36 - Azul claro             46 - Azul claro
                                 37 - Cinza                  47 - Cinza
 
 """                 
+print('\033[31mOlá Mundo!')             # Olá mundo em vermelho.
+print('\033[31;44mOlá Mundo!\033[m')    # Olá mundo com texto vermelho, fundo azul claro só onde tem texto.
+print('\033[1;32;43mOlá Mundo!')        # Olá mundo com texto estilo negrito, texto verde e fundo amarelo.
+print('\033[4;30;45mOlá Mundo!\033[m')  # Olá mundo sublinhado, texto branco e fundo roxo.
