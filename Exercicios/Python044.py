c6bee61cdf5335a7194663e923dc88a94f14ea0e
Conrado Bolonha <conradobolonha@gmail.com>
2022-04-20 16:47:56 -0300

    v.1.00

---
+++ Exercicios/Python044.py
@@ -0,0 +1,10 @@
+"""
+Elabore um programa que calcule o valor a ser pago por um produto,
+considerando o seu preço normal e condição de pagamento:
+
+* À vista: dinheiro/cheque: 10% de desconto.
+* À vista no cartão de débito: 5% de desconto.
+* Em até duas vezes no cartão de crédito: preço normal.
+* Três vezes ou mais no cartão de crédito: 20% de juros.
+"""
+
