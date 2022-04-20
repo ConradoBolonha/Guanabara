29ccae0b35cbc43cf8468b4da1b6299ab6f7b519
Conrado Bolonha <conradobolonha@gmail.com>
2022-04-20 16:42:59 -0300

    v.1.00

---
+++ Exercicios/Python041.py
@@ -0,0 +1,13 @@
+"""
+A Confederação Nacioanal de Natação precisa de um programa
+que leia o ano de nascimento de um atleta e mostre sua 
+categoria de acordo com a idade:
+
+* Até 9 anos: MIRIM
+* De 10 a 14 anos: INFANTIL
+* De 15 a 19 anos: JUNIOR
+* Com 20 anos: SÊNIOR
+* Acima de 21 anos: MASTER
+    
+"""
+
---
+++ Exercicios/Python042.py
@@ -0,0 +1,9 @@
+"""
+Refaça o desafio 35 dos triângulo, acrescentando o recurso de mostrar
+que tipo de triângulo será formado:
+
+* Equilátero: Todos os lados são iguais.
+* Isóceles: Dois lados iguais.
+* Escaleno: Todos os lados são diferentes.
+"""
+
