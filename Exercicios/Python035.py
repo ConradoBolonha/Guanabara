a68e9688f62af69405ab7226b892dd685b01c501
Conrado Bolonha <conradobolonha@gmail.com>
2022-04-20 17:37:45 -0300

    v.1.00

--- Exercicios/Python035.py
+++ Exercicios/Python035.py
@@ -2,7 +2,14 @@
 Desenvolva um algoritmo que leia o comprimento de três retas e
 diga ao usuário se elas podem ou não formar um triângulo.
 """
-
+print('\n')
 print('-=' * 17)
 print(' *** ANALISADOR DE TRIÂNGULOS ***')
 print('-=' * 17)
+r1 = float(input('Tamanho do primeiro segmento: '))
+r2 = float(input('Tamanho do segundo segmento: '))
+r3 = float(input('Tamanho do terceiro segmento: '))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('Os segmentos acima FORMAM um triângulo!')
+else:
+    print('Os segementos acima NÃO FORMAM um triângulo!!!')
