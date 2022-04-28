c3ed8abc97182e4b1b03eea5f7544acc6e100992
Conrado Bolonha <conradobolonha@gmail.com>
2022-04-28 14:45:12 -0300

    v.1v00

---
+++ Conversor_Horas.py
@@ -0,0 +1,19 @@
+
+print('=-' * 5, 'CONVERSOR DE HORAS, MINUTOS E SEGUNDOS', '=-' * 5)
+print('''\nEscolha uma opção para ver sua conversão:
+[ 1 ] - HORAS em MINUTOS
+[ 2 ] - HORAS em SEGUNDOS
+[ 3 ] - MINUTOS em SEGUNDOS''')
+opcao = int(input('\nDigite uma das opções de conversão: '))
+if opcao == 1:
+    horas = int(input('Digite a quantidade de HORAS: '))
+    horas_minutos = 60 * horas    
+    print('\nNa conversão de {} hora(s), temos {} minutos.'.format(horas, horas_minutos))
+elif opcao == 2:
+    horas = int(input('Digite a quantidade de HORAS: '))
+    horas_segundos = 3600 * horas
+    print('\nNa conversão de {} hora(s), temos {} segundos.'.format(horas, horas_segundos))
+elif opcao == 3:
+    minutos = int(input('Digite a quantidade de MINUTOS: '))
+    minutos_segundos = minutos * 60
+    print('\nNa conversão de {} minutos, temos {} segundos.'.format(minutos, minutos_segundos))
--- Exercicios/TEMP.py
+++
@@ -1,8 +0,0 @@
-"""
-from datetime import date
-ano = print(date.today().year
- = print(date.today().month
-dia = print(date.today().day
-print('Hoje é {} de {} de {}.'.format(dia, mes, ano))
-"""
-
