2d5886d973379445d84d476c18cf8efa3e06a8c8
Conrado Bolonha <conradobolonha@gmail.com>
2022-05-04 16:19:32 -0300

    v.1.00

--- Exercicios/Python045.py
+++ Exercicios/Python045.py
@@ -3,6 +3,7 @@ Crie um jogo que faça o computador jogar Jokenpô com você.
 """
 from random import randint
 from time import sleep
+
 print ('\n{:*^40}'.format(' JOGO DE JOKENPÔ '))
 itens = ('PEDRA', 'PAPEL', 'TESOURA')
 computador = randint(0, 2)
@@ -19,30 +20,44 @@ sleep(1)
 print('PÔ !!!')
 print('\nO computador jogou {}.'.format(itens[computador]))
 print('O jogador jogou {}.'.format(itens[jogador]))
-print('=-' * 13)
-if computador == 0: #  PEDRA1
-        print('\nO JOGADOR VENCEU :-)')
-    elif jogador == 2: #  TESOURA
-        print('\nO COMPUTADOR GANHOU :-(')
-    else:
-        print('\nJOGADA INVÁLIDA')
-    
-elif computador == 1: #  PAPEL
-    if jogador == 0: #  PEDRA
-        print('\nO COMPUTADOR VENCEU :-(')
+#  print('=-' * 13)
+if computador == 0: #  PEDRA
+    if jogador == 0: # PEDRA
+        print('\nEMPATE!!!')
+        print('\n')
     elif jogador == 1: #  PAPEL
-        print('\nEMPATE')
+        print('\nO JOGADOR VENCEU :-)')
+        print('\n')
+    elif jogador == 2: # TESOURA
+        print('\nO JOGADOR PERDEU :-(')
+        print('\n')
+    else:
+        print('JOGADA INVÁLIDA!!!')
+        print('\n')
+        
+elif computador == 1: #  PAPEL
+    if jogador == 0: # PEDRA
+        print('\nO JOGADOR PERDEU :-(')
+        print('\n')
+    elif jogador == 1: #  PAPEL
+        print('\nEMPATE!!!')
+        print('\n')
     elif jogador == 2: #  TESOURA
         print('\nO JOGADOR VENCEU :-)')
+        print('\n')
     else:
-        print('\nJOGADA INVÁLIDA')
-
+        print('\nJOGADA INVÁLIDA!!!')
+        print('\n')
 elif computador == 2: #  TESOURA
     if jogador == 0: #  PEDRA
         print('\nO JOGADOR VENCEU :-)')
+        print('\n')
     elif jogador == 1: #  PAPEL
-        print('\nO COMPUTADOR VENCEU :-(')
-    elif jogador == 2: #  TESOURA
-        print('\nEMPATE')
+        print('\nO JOGADOR PERDEU :-(')
+        print('\n')
+    elif jogador == 2:
+        print('\nEMPATE!!!')
+        print('\n')
     else:
-        print ('\nJOGADA INVÁLIDA')
+        print('\nJOGADA INVÁLIDA!!!')
+        print('\n')
