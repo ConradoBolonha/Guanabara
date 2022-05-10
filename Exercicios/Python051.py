da29a44d5753cda5dde3603731bdb93b79290dbc
Conrado Bolonha <conradobolonha@gmail.com>
2022-05-10 11:57:17 -0300

    v.1.00

--- Exercicios/Python051.py
+++ Exercicios/Python051.py
@@ -4,3 +4,12 @@ de uma PA (Progressão Aritmética). No final, mostre os 10 primeiros termos
 dessa progressão.
 """
 
+print('=' * 20)
+print('10 TERMOS DE UMA PA')
+print('=' * 20)
+prim_termo = int(input('Primeiro termo: ')) #  Número pelo qual se dará o início da contagem
+razao = int(input('Razão: ')) # Se razão = 5, irá pular de 5 em 5 por exemplo.
+decimo_termo = prim_termo + (10 - 1) * razao
+for cont in range(prim_termo, decimo_termo + razao, razao):
+    print('{} '.format(cont), end = ' -> ')
+print('FIM')
--- Exercicios/Python052.py
+++ Exercicios/Python052.py
@@ -1,5 +1,19 @@
 """
 Faça um programa que leia um número inteiro e diga se ele
-é ou não um núemro primo.
+é ou não um número primo.
 """
 
+NumPrimo = int(input('Digite um número: '))
+total = 0
+for cont in range(1, NumPrimo + 1):
+    if NumPrimo % cont == 0:
+        print('\033[31m', end = ' ')
+        total += 1
+    else:
+        print('\033[36m', end = ' ')
+    print('{}'.format(cont), end = ' ')
+print('\nO número {} foi divisível por {}.'.format(NumPrimo, total))
+if total ==2:
+    print('O número {} É primo!'.format(NumPrimo))
+else:
+    print('O número {} NÃO é primo!'.format(NumPrimo))
