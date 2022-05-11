bb1fb49d34683c14eb794e3cc637f19ffd21842b
Conrado Bolonha <conradobolonha@gmail.com>
2022-05-11 17:19:29 -0300

    v.1.00

--- Exercicios/Python055.py
+++ Exercicios/Python055.py
@@ -2,7 +2,16 @@
 Faça um programa que leia o peso de cinco pessoas e mostre no
 final, qual foi o maior e menor peso lidos.
 """
-
+maior_peso = 0
+menor_peso = 0
 for pessoas in range(1, 6):
-    peso = float(input('Peso da {}ª pessoa: '.format(pessoas)))
-print(peso)
+    peso = float(input('\nPeso da {}ª pessoa: '.format(pessoas)))
+    if pessoas == 1:
+        maior_peso = peso
+        menor_peso = peso
+    else:
+        if peso > maior_peso:
+            maior_peso = peso
+        if peso < menor_peso:
+            menor_peso = peso
+print('O maior peso é de {} e o menor é {}'.format(maior_peso, menor_peso))
--- Exercicios/Python056.py
+++ Exercicios/Python056.py
@@ -1,5 +1,5 @@
 """
-Leia um programa que leia o nome, idade e sexo de uma pessoa
+Leia um programa que leia o nome, idade e sexo de 4 pessoas
 e que no final o programa mostre:
 
 * A média de idade do grupo;
@@ -7,3 +7,26 @@ e que no final o programa mostre:
 * Quantos mulheres tem menos de 21 anos.
 """
 
+SomaIdade = 0
+MediaIdade = 0
+MaiorIdadeHomem = 0
+NomeVelho = ''
+TotalMulher20 = 0
+for pessoa in range(1, 5):
+    print('\n{}ª pessoa: '.format(pessoa))
+    nome = str(input('Nome: ')).strip()
+    idade = int(input('Idade: '))
+    sexo = str(input('Sexo [M/F]: ')).strip()
+    SomaIdade += idade
+    if pessoa == 1 and sexo in 'Mn':
+        MaiorIdadeHomem = idade
+        NomeVelho = nome
+    if sexo in 'Mn' and idade > MaiorIdadeHomem:
+        MaiorIdadeHomem = idade
+        NomeVelho = nome
+    if sexo in 'Ff' and idade < 20:
+        TotalMulher20 += 1
+MediaIdade = SomaIdade / 4
+print('A média de idade do grupo é de {} anos.'.format(MediaIdade))
+print('O homem mais velho tem {} anos e se chama {}'.format(MaiorIdadeHomem, NomeVelho))
+print('Ao todo, são {} mulheres com menos de 20 anos.'.format(TotalMulher20))
