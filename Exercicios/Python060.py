dfbac98f4ac1a7827b4af3e89443bb5ac8180f94
Conrado Bolonha <conradobolonha@gmail.com>
2022-05-17 17:23:16 -0300

    v.1.00

--- Exercicios/Python060.py
+++ Exercicios/Python060.py
@@ -4,4 +4,22 @@ seu fatorial.
 
 Exemplo: 5! = 5x4x3x2x1 = 120
 '''
+'''
+print('\n=== CALCULANDO O FATORIAL DE UM NÚMERO ===')
+from math import factorial
+numero = int(input('\nDigite um número: '))
+fatorial = factorial(numero)
+print('O fatorial de {} é {}.\n'.format(numero, fatorial))
+'''
 
+print('\n=== CÁLCULO DO FATORIAL DE UM NÚMERO ===')
+numero = int(input('\nDigite um número: '))
+contador = numero
+fatorial = 1
+print('\nO fatorial de {} ='.format(numero), end = ' ')
+while contador > 0:
+    print('{}'.format(contador), end = ' ')
+    print('x ' if contador > 1 else '=', end = '')
+    fatorial = fatorial * contador
+    contador = contador - 1
+print(' {}'.format(fatorial), '\n')
--- Exercicios/Python061.py
+++ Exercicios/Python061.py
@@ -3,3 +3,13 @@ Refaça o desafio 051 lendo o primeiro termo e a razão de uma PA (progressão a
 mostrando os 10 primeiro termos da progressão usando a estrutura while.
 '''
 
+print('\n','-=' * 8, 'GERADOR DE TERMO', '=-' * 10)
+primeiro_termo = int(input('\nDigite o primeiro termo: '))
+razao = int(input('Digite a razão da PA: '))
+termo = primeiro_termo
+contador = 1
+while contador <= 10:
+    print('{} -> '.format(termo), end = '')
+    termo = termo + razao
+    contador = contador+ 1
+print('FIM', '\n')
