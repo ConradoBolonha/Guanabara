084efa524ddb3f7c8706f606c6708e0848d8c9f6
Conrado Bolonha <conradobolonha@gmail.com>
2022-05-17 11:58:54 -0300

    v.1.00

--- Exercicios/Python059.py
+++ Exercicios/Python059.py
@@ -9,7 +9,7 @@ Crie um algoritmo que leia dois valores e mostre um menu na tela:
 
 Seu programa deverá realizar a operação solicitada em cada caso.
 '''
-
+from time import sleep
 valor1 = int(input('\nDigite um valor inteiro: '))
 valor2 = int(input('Digite o segundo valor: '))
 opcao = 0
@@ -27,18 +27,20 @@ while opcao != 5:
         print('A soma entre {} e {} é {}.'.format(valor1, valor2, soma))
     elif opcao == 2:
         multiplicacao = valor1 * valor2
-        print('A subtração entre {} e {} é {}.'.format(valor1, valor2, multiplicacao))
+        print('A multiplicação entre {} e {} é {}.'.format(valor1, valor2, multiplicacao))
     elif opcao == 3:
         if valor1 > valor2:
             maior = valor1
         else:
             maior = valor2
+        print('Entre {} e {}, o maior valor é {}.'.format(valor1, valor2, maior))
     elif opcao == 4:
-        print = ('Digite novamente: ')
+        print('Digite novamente! ')
         valor1 = int(input('Digite um valor inteiro: '))
         valor2 = int(input('Digite o segindo valor: '))
     elif opcao == 5:
         print('Programa encerrado!')
     else:
-        print('Opção inválida: Digite novamente!')
-print('\n*** FIM ***')
+        print('\n<<< Opção inválida. Digite novamente! >>>')
+    sleep(2)
+print('\n*** FIM ***', '\n')
