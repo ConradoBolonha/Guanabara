fdc09ccc219709031e3e3d456546d4f451f44a77
Conrado Bolonha <conradobolonha@gmail.com>
2022-05-18 16:39:03 -0300

    v.1.00

--- Exercicios/Python062.py
+++ Exercicios/Python062.py
@@ -3,3 +3,20 @@ Melhore o desafio 061, perguntando para o usuário se ele quer mostrar mais algu
 programa encerra quando ele disser que quer mostrar 0 termos.
 '''
 
+print('\n','-=' * 8, 'GERADOR DE TERMO', '=-' * 10)
+primeiro_termo = int(input('\nDigite o primeiro termo: '))
+razao = int(input('Digite a razão da PA: '))
+termo = primeiro_termo
+contador = 1
+total = 0
+novos_termos = 10
+while novos_termos != 0:
+    total = total + novos_termos
+    while contador <= total:
+        print('{} -> '.format(termo), end = '')
+        termo = termo + razao
+        contador = contador + 1
+    print('PAUSA', '\n')
+    novos_termos = int(input('Quantos termos a mais deseja mostrar? '))
+print('\nA progressão executada mostrou {} termos.'.format(total))
+print('*** FIM ***')
--- Exercicios/Python063.py
+++ Exercicios/Python063.py
@@ -4,4 +4,17 @@ elementos de uma Sequência de Fibonacci.
 
 Exemplo: 0 -> 1 -> 1 -> 2 -> 3 -> 5 -> 8
 '''
-
+print('\n', '=-' * 8, 'SEQUÊNCIA DE FOBONATTI', '=-' * 8)
+n = int(input('\nDigite a quantidade de termo Fibonacci: '))
+termo_1 = 0
+termo_2 = 1
+print('~' * 15)
+print('{} -> {}'.format(termo_1, termo_2), end = '')
+contador = 3
+while contador <= n:
+    termo_3 = termo_1 + termo_2
+    print(' -> {}'.format(termo_3), end = '')
+    termo_1 = termo_2
+    termo_2 = termo_3
+    contador = contador + 1
+print(' -> FIM')
