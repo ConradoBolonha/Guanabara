11df66a8cae27d22842b7ccdeaa3c5151f680d77
Conrado Bolonha <conradobolonha@gmail.com>
2022-05-25 14:42:50 -0300

    v.1.00

--- Exercicios/Python070.py
+++ Exercicios/Python070.py
@@ -8,13 +8,29 @@ b) Quantos produtos custam mais de R$ 1.000,00;
 c) Qual é o nome do produto mais barato.
 '''
 
+print('-' * 22, '\nLOJAS TEM TUDO', '\n', '-' * 22)
+total_prod = total_prod_mil = menor_prod = cont_prod = 0
+nome_prod_barato = ' '
 while True:
     nome_prod = str(input('Digite o nome do produto: '))
-    valor_prod = float(input('Digite o valor: '))
-    
+    valor_prod = float(input('Digite o valor: R$ '))
+    total_prod += valor_prod
+    cont_prod += 1
+    if valor_prod > 1000:
+        total_prod_mil += 1
+    if cont_prod == 1: # Se o contador for o primeiro produto (1 = primeiro produto)
+        menor_prod = valor_prod
+        nome_prod_barato = nome_prod   
+    else:
+        if  valor_prod < menor_prod:
+            menor_prod = valor_prod
+            nome_prod_barato = nome_prod
     resposta = ' '
     while resposta not in 'SN':
-        resposta = str(input('Incluir mais produtos? [S/N] } ')).strip().upper() [0]
+        resposta = str(input('Incluir mais produtos? [S/N] ')).strip().upper() [0]
     if resposta == 'N':
         break
+print(f'O valor total dos gastos é R$ {total_prod}')
+print(f'{total_prod_mil} produtos custam mais de R$ 1.000,00')
+print(f'{nome_prod_barato} foi o produto mais barato(R$ {menor_prod}).')
 print(('\n'), '{:-^40}'.format('FIM'))
--- Exercicios/Python071.py
+++ Exercicios/Python071.py
@@ -4,6 +4,32 @@ No início, pergunte ao usuário qual será o valor a ser sacado
 (número inteiro) e o programa vai informar quantas cédulas de
 cada valor serão entregues.
 
-Obs: Considere que o caixa possui cédulas de R$ 50,00 / R$ 20,00 / R$ 10,00 / R$ 1,00 
+Obs: Considere que o caixa possui cédulas
+de R$ 50,00 / R$ 20,00 / R$ 10,00 / R$ 5,00 / R$ 1,00 
 '''
-
+print('=' * 29)
+print('{:^30}'.format('SIMULADOR DE CAIXA ELETRÔNICO'))
+print('=' * 29)
+valor = int(input('Digite o valor do saque: R$ '))
+total = valor
+cedula = 50
+total_ced = 0
+while True:
+    if total >= cedula:
+        total -= cedula
+        total_ced += 1
+    else:
+        if total_ced > 0:
+            print(f'Total de {total_ced} cédulas de R$ {cedula}')
+        if cedula == 50:
+            cedula = 20
+        elif cedula == 20:
+            cedula = 10
+        elif cedula == 10:
+            cedula = 5
+        elif cedula == 5:
+            cedula = 1
+        total_ced = 0
+        if total == 0:
+            break
+print('\n','*** SESSÃO ENCERRADA ***')
